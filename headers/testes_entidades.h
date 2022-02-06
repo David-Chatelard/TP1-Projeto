900ad1021a36f96fa48b42123dd82f76c3b3f9c6
David Fanchic Chatelard <davidfchatelard@gmail.com>
2022-02-06 11:48:05 -0300

    Tirei os static das strings

--- headers/testes_entidades.h
+++ headers/testes_entidades.h
@@ -3,6 +3,8 @@
 
 #include "entidades.h"
 
+#include <string>
+
 using namespace std;
 
 /*----------------------------------------------------------------------------------------------
@@ -29,9 +31,9 @@ public:
 
 class TUUsuario {
 private:
-    inline const static string VALOR_VALIDO_NOME    = "Alexandre Cunha.";   
-    inline const static string VALOR_VALIDO_EMAIL   = "Email_Teste.123@gmail.com";   
-    inline const static string VALOR_VALIDO_SENHA   = "BfA843";
+    const string VALOR_VALIDO_NOME    = "Alexandre Cunha.";   
+    const string VALOR_VALIDO_EMAIL   = "Email_Teste.123@gmail.com";   
+    const string VALOR_VALIDO_SENHA   = "BfA843";
     Usuario *usuario;                       
     int estado;                             
     void setUp();                          
@@ -45,9 +47,9 @@ public:
 
 class TUAvaliacao {
 private:
-    inline const static string VALOR_VALIDO_CODIGO    = "45670123";   
+    const string VALOR_VALIDO_CODIGO    = "45670123";   
     const static int VALOR_VALIDO_NOTA   = 0;   
-    inline const static string VALOR_VALIDO_DESCRICAO = "O aluno foi péssimo e infelizmente vai reprovar.";
+    const string VALOR_VALIDO_DESCRICAO = "O aluno foi pessimo e infelizmente vai reprovar.";
     Avaliacao *avaliacao;                       
     int estado;                             
     void setUp();                          
@@ -61,10 +63,10 @@ public:
 
 class TUSessao {
 private:
-    inline const static string VALOR_VALIDO_CODIGO    = "52894220";   
-    inline const static string VALOR_VALIDO_DATA      = "12-Out-2027";   
-    inline const static string VALOR_VALIDO_HORARIO   = "00:00";
-    inline const static string VALOR_VALIDO_IDIOMA    = "Chines Mandarim";
+    const string VALOR_VALIDO_CODIGO    = "52894220";   
+    const string VALOR_VALIDO_DATA      = "12-Out-2027";   
+    const string VALOR_VALIDO_HORARIO   = "00:00";
+    const string VALOR_VALIDO_IDIOMA    = "Chines Mandarim";
     Sessao *sessao;                       
     int estado;                             
     void setUp();                          
@@ -78,13 +80,13 @@ public:
 
 class TUExcursao {
 private:
-    inline const static string VALOR_VALIDO_CODIGO      = "12344321";   
-    inline const static string VALOR_VALIDO_TITULO      = "Trilha Chinesa Mística";   
+    const string VALOR_VALIDO_CODIGO      = "12344321";   
+    const string VALOR_VALIDO_TITULO      = "Trilha Chinesa Mistica";   
     const static int VALOR_VALIDO_NOTA     = 3;
-    inline const static string VALOR_VALIDO_CIDADE      = "Bangkok";
+    const string VALOR_VALIDO_CIDADE      = "Bangkok";
     const static int VALOR_VALIDO_DURACAO  = 180;
-    inline const static string VALOR_VALIDO_DESCRICAO   = "Venha descobrir os mistérios chineses!";
-    inline const static string VALOR_VALIDO_ENDERECO    = "A ser definido";
+    const string VALOR_VALIDO_DESCRICAO   = "Venha descobrir os misterios chineses!";
+    const string VALOR_VALIDO_ENDERECO    = "A ser definido";
     Excursao *excursao;                       
     int estado;                             
     void setUp();                          
