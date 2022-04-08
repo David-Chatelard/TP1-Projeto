8c8cdd32439d87e39ca98ea0f86ba0b5c7c0f520
David Fanchic Chatelard <davidfchatelard@gmail.com>
2022-04-08 19:57:26 -0300

    Update controladorasservico.cpp

--- source/controladorasservico.cpp
+++ source/controladorasservico.cpp
@@ -15,18 +15,12 @@ bool CntrServicoAutenticacao::autenticar(Email email, Senha senha) {
         //Comparar senha informada com a senha recuperada.
 
         if(senhaRecuperada == senha.getValor()) {
-            cout << "senha certa ENTER para continuar...";
-            getch();
             return true;
         }
-        cout << "senha errada ENTER para continuar...";
-        getch();
         return false;
     }
     catch(EErroPersistencia &exp) {
         cout << endl << exp.what();
-        cout << "erro no sql ENTER para continuar...";
-        getch();
         return false;
     }
 }
