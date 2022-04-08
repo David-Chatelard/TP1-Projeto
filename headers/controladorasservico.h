c62296777687c19059e71cb1e839c14a68791a4c
David Fanchic Chatelard <davidfchatelard@gmail.com>
2022-04-08 16:50:34 -0300

    Criação da controladorasservico

---
+++ headers/controladorasservico.h
@@ -0,0 +1,13 @@
+#ifndef CONTROLADORASSERVICO_H_INCLUDED
+#define CONTROLADORASSERVICO_H_INCLUDED
+
+#include <string.h>
+
+#include "dominios.h"
+#include "entidades.h"
+#include "interfaces.h"
+
+using namespace std;
+
+
+#endif
